3b597b77b298374b68d173348850c20b58f1ab09
Rafael Eufrázio <73612516+RafaelEufrazio@users.noreply.github.com>
2024-06-16 23:27:45 -0300

    fix: removed weird space

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Item(BaseModel):
 def read_root():
     return {"Hello": "World"}
 
-@app.get(   "/items/{item_id}")
+@app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
