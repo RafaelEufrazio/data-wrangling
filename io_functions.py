3e8c4de42547dfe4f6529b2f85295e338b462d7f
Rafael Eufrázio <73612516+RafaelEufrazio@users.noreply.github.com>
2024-06-29 17:34:14 -0300

    feat: added function to convert back to csv

--- io_functions.py
+++ io_functions.py
@@ -6,12 +6,30 @@ import io
 class FileType(Enum):
     CSV = 1
 
-class OpenArgs():
+class IoArgs():
     file_type: Type[FileType]
     decimal: str
     sep: str
+    
+    def __init__(self, file_type: Type[FileType], decimal: str, sep: str):
+        self.file_type = file_type
+        self.decimal = decimal
+        self.sep = sep
 
-def open(source: io.StringIO, rules: OpenArgs) -> pd.DataFrame:
+def open(source: io.StringIO, rules: IoArgs) -> pd.DataFrame:
     match rules.file_type:
         case FileType.CSV:
             return pd.read_csv(dir=source, decimal=rules.decimal, sep=rules.sep)
+        
+        
+        
+def convert(dataframe: pd.DataFrame, rules: IoArgs) -> any: # Figure out how to type this properly
+    match rules.file_type:
+        case FileType.CSV:
+            return convert_to_csv(dataframe=dataframe, rules=rules)
+            
+            
+def convert_to_csv(dataframe: pd.DataFrame, rules: IoArgs) -> io.StringIO:
+    csv = io.StringIO()
+    dataframe.to_csv(csv)
+    return csv
